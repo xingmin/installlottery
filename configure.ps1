c20ea609520fa05159b69776df0fbc456370210d
xingmin liu <xingminliu@qq.com>
2013-07-31 23:06:08 +0800

    add submodule for config the basic env and add some code to configure.ps1 about packing sp3

--- configure.ps1
+++ configure.ps1
@@ -1,3 +1,4 @@
+#设置电源管理
 Write-Host -NoNewline "设置电源管理为[一直开着]:"
 $powerstyle = "一直开着"
 $m0 = powercfg.exe -List | select-string -Pattern  "$powerstyle"
@@ -30,6 +31,18 @@ if ($m0 -ne $null){
 	}
 	Write-Host -NoNewline "OK."
 }
-
+#禁用屏保
 Write-Host "禁用屏幕保护程序(系统重启后生效):OK"
 Set-ItemProperty -Path "Registry::HKEY_CURRENT_USER\Control Panel\Desktop" -Name "ScreenSaveActive" -Value 0
+
+#安装sp3补丁
+$version = Get-WmiObject -Class Win32_OperatingSystem |  select-object -expandproperty version
+$ver = $version.split(".")
+$v = [int]$ver[0]*10000000+[int]$ver[1]*100000+[int]$ver[2]
+[int]$vsp3 = 50102600
+if (([int]$ver[0] -eq 5) -and ($v -lt $vsp3)){
+	#如果xp版本低于sp3将安装sp3补丁
+	Write-Host "xxx"
+}else{
+	Write-Host "操作系统版本高于XPSP3"
+}
